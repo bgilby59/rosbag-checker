1b14e4e211c44a0ad24f4c4c4f2624a0a2313a14
benjamin-gilby <benjamin.gilby@tier4.jp>
2023-05-30 11:22:28 -0400

    Change name of node file and executable name

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rosbag_checker_node = rosbag_checker.rosbag_checker_node:main'
+            'rosbag_checker = rosbag_checker.rosbag_checker_node:main'
         ],
     },
 )
